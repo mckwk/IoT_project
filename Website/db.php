31f2113fff5ea10e61dedc6f163de4888558cbd3
Rémy Lempire <remy.lempire@student.umons.ac.be>
2025-03-21 09:46:42 +0100

    modify (db.php): updating a syntax mistake

--- Website/db.php
+++ Website/db.php
@@ -7,8 +7,8 @@ $password = "pwd";
 
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require", $user, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION;
-        PDO::ATTR_EMULATE_PREPARES => false                  
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false
     ]);
 } catch (PDOException $e) {
     die("Database connection error: " . $e->getMessage());
