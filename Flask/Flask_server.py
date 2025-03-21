94c4a962377291e5ebd026d7233397fc77ea7b95
Mathieu1704 <121064189+Mathieu1704@users.noreply.github.com>
2025-03-21 11:08:34 +0100

    set up of hashing password

--- Flask/Flask_server.py
+++ Flask/Flask_server.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import psycopg2
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 DATABASE_URL = "url"
 
@@ -54,6 +56,46 @@ def store_data():
         return "Data stored successfully", 200
     else:
         return "Database error", 500
+    
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    email = data['email']
+    password = data['password']
+
+    hashed_pw = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (email, password) VALUES (%s, %s)", (email, hashed_pw))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return jsonify({"message": "Utilisateur inscrit avec succès !"}), 201
+    except Exception as e:
+        print("Database error:", e)
+        return jsonify({"error": "Database error"}), 500
+    
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    email = data['email']
+    password = data['password']
+
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        cursor = conn.cursor()
+        cursor.execute("SELECT password FROM users WHERE email = %s", (email,))
+        user = cursor.fetchone()
+
+        if user and bcrypt.check_password_hash(user[0], password):  # Vérification du mot de passe
+            return jsonify({"message": "Connexion success !"}), 200
+        else:
+            return jsonify({"error": "Email or password incorrect"}), 401
+    except Exception as e:
+        print("Database error:", e)
+        return jsonify({"error": "Database error"}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
