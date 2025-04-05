df747371dc6ed31634216458d7a8803b6f4204eb
ffasterss <126521233+ffasterss@users.noreply.github.com>
2025-04-05 19:08:39 +0200

    Update register.php

--- website/register.php
+++ website/register.php
@@ -25,6 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body class="bg-light">
 <div class="container mt-5">
